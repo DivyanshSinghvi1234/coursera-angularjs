2fd1989a2629bdf2a488893f35e505a49d802a6f
DivyanshSinghvi1234 <singhvidivyansh7885@gmail.com>
2025-04-12 17:43:39 +0530

    Update categories.component.js

--- module4-solution/categories.component.js
+++ module4-solution/categories.component.js
@@ -4,12 +4,13 @@
     templateUrl: 'categories.html',
     controller: CategoriesController,
     bindings: {
-      categories: '<'
+      categories: '<'  // Use one-way data binding to pass categories to the component
     }
   });
 
   CategoriesController.$inject = [];
   function CategoriesController() {
     var $ctrl = this;
+    // Categories will be available as $ctrl.categories
   }
 })();
